21f73bab6d6ed69bc0567a4d99c33945249e6f8e
TheAnnoying <drork2018@gmail.com>
2023-07-29 00:00:23 +0300

    Created ping command

---
+++ commands/ping.js
@@ -0,0 +1,20 @@
+export default {
+    name: "ping",
+    category: "bot",
+    description: "Check the ping",
+    async execute(message, args, client) {
+        const before = Date.now();
+        const ping = await message.reply({ embeds: [ fn.makeEmbed({
+            title: "Pinging...",
+            field: [ "API Latency", `${client.ws.ping}ms` ],
+        }) ], fetchReply: true });
+
+        ping.edit({ embeds: [ fn.makeEmbed({
+            title: "Ping Results",
+            fields: [
+                [ "API Latency", `${client.ws.ping}ms` ],
+                [ "Bot Latency", `${Date.now() - before}ms` ]
+            ]
+        }) ] });
+    }
+}
