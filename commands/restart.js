c0292adccdf4afb5b7be8aa961032a2d0a3aaa5b
TheAnnoying <drork2018@gmail.com>
2023-07-29 00:00:13 +0300

    Added category, description and made owner only

--- commands/restart.js
+++ commands/restart.js
@@ -2,15 +2,17 @@ import fs from "node:fs";
 
 export default {
     name: "restart",
-    aliases: [ "reboot", "r" ],
+    category: "bot",
+    description: "Restart the bot",
+    owner: true,
+    aliases: [ "reboot", "end", "kill" ],
     async execute(message, args, client) {
-        await client.application.fetch();
-        if(message.author.id !== client.application.owner.id) return message.reply({ embeds: [ fn.makeError("This command can only be used by the bot's owner") ] });
-
         const msg = await message.reply({ embeds: [ fn.makeEmbed({ description: "Restarting..." }) ] });
 
         client.destroy();
-        fs.writeFileSync("./data/restart.json", JSON.stringify([msg.channel.id, msg.id]), "utf8");
+        if(["reboot", "restart"].includes(message.content)) {
+            fs.writeFileSync("./data/restart.json", JSON.stringify([msg.channel.id, msg.id]), "utf8");
+        }
         process.exit();
     }
 }
