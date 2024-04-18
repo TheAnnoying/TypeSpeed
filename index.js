2c2c286e8c115eec8193bdbb53f13be8ea46a66d
TheAnnoying <drork2018@gmail.com>
2024-04-18 22:39:49 +0300

    feat!: supports slash commands

--- index.js
+++ index.js
@@ -72,7 +72,9 @@ client.once(Discord.Events.ClientReady, async () => {
 });
 
 client.commands = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
 client.commandList = [];
+client.slashCommandsData = [];
 
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 for (const file of commandFiles) {
@@ -80,6 +82,19 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
     if(!command.owner) client.commandList.push(command);
     for (const alias of command.aliases ?? []) client.commands.set(alias, command);
+}
+
+const slashCommandFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"));
+
+for (const file of slashCommandFiles) {
+    let command = (await import(`./slash/${file}`)).default;
+    client.slashCommands.set(command.name, command);
+
+    let strippedCommand = JSON.parse(JSON.stringify(command));
+    strippedCommand.description = locale["en"].commands[strippedCommand.name].info.description;
+    strippedCommand.execute = null;
+
+    client.slashCommandsData.push(strippedCommand);
 }
 
 client.on(Discord.Events.MessageCreate, async message => {
@@ -106,89 +121,99 @@ client.on(Discord.Events.MessageCreate, async message => {
             if(args.length === 1 && ["help", "h"].includes(args[0])) {
                 message.content = `t!help ${command.name}`;
                 client.commands.get("help").execute(message, [ command.name ]);
-            } else command.execute(message, args, client);
+            } else command.execute(message, args);
         };
     }
 });
 
 client.on(Discord.Events.InteractionCreate, async interaction => {
     const lang = fn.getLang(interaction);
+    if(interaction.isCommand()) {
+        const command = client.slashCommands.get(interaction.commandName);
+        command.execute(interaction);
+    } else if(interaction.isAutocomplete()) {
+        const command = client.slashCommands.get(interaction.commandName);
+        command?.autocomplete?.(interaction);
+    } else {
+        if(interaction.customId.startsWith("delete")) {
+            if(interaction.member.user.id !== fn.db.tests.getTestFromId(interaction.customId.split("_")[1]).user) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.deletetest.testmakeronly, interaction) ] });
+            fn.db.tests.removeTestById(interaction.customId.split("_")[1]);
+    
+            interaction.message.edit({ components: [ fn.makeRow({ buttons: [{ label: locale[lang].buttons.deletetest.label, id: `delete`, style: "danger", disabled: true }] }) ] })
+            interaction.reply({ ephemeral: true, embeds: [ fn.makeEmbed({ description: locale[lang].buttons.deletetest.description, title: locale[lang].buttons.deletetest.title }) ] })
+        }
 
-    if(interaction.customId.startsWith("delete")) {
-        if(interaction.member.user.id !== fn.db.tests.getTestFromId(interaction.customId.split("_")[1]).user) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.deletetest.testmakeronly, interaction) ] });
-        fn.db.tests.removeTestById(interaction.customId.split("_")[1]);
+        if(interaction.customId.startsWith("nevermind")) {
+            if(interaction.member.user.id !== interaction.customId.split("_")[1]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] });
+            interaction.deferUpdate();
+    
+            interaction.message.delete();
+            (await fn.getMessage(interaction.message.channel, interaction.message.reference.messageId)).delete()
+        }
 
-        interaction.message.edit({ components: [ fn.makeRow({ buttons: [{ label: locale[lang].buttons.deletetest.label, id: `delete`, style: "danger", disabled: true }] }) ] })
-        interaction.reply({ ephemeral: true, embeds: [ fn.makeEmbed({ description: locale[lang].buttons.deletetest.description, title: locale[lang].buttons.deletetest.title }) ] })
-    }
-    if(interaction.customId.startsWith("nevermind")) {
-        if(interaction.member.user.id !== interaction.customId.split("_")[1]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] });
-        interaction.deferUpdate();
+        if(interaction.customId.startsWith("run_")) {
+            if(interaction.member.user.id !== interaction.customId.split("_")[2]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] }); 
+            interaction.deferUpdate();
+    
+            const msg = await fn.getMessage(interaction.message.channel, interaction.message.reference.messageId);
+            const args = msg.content.slice("t!".length).split(" ");
+            const providedCommand = interaction.customId.split("_")[1].toLowerCase();
+            args.shift();
+            
+            const command = client.commands.get(providedCommand);
+    
+            if(args.length === 1 && ["help", "h"].includes(args[0])) {
+                msg.content = `t!help ${command.name}`;
+                client.commands.get("help").execute(msg, [ command.name ]);
+            } else command.execute(msg, args);
+    
+            interaction.message.delete();
+        }
+    
+        if(interaction.customId.startsWith("lang")) {
+            const lang = fn.getLang(interaction);
+            const newLang = interaction.values[0];
+    
+            if(interaction?.guild) { 
+                if(interaction.member.user.id !== interaction.customId.split("_")[1]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] }); 
+    
+                fn.db.guilds.set(interaction?.guild?.id, newLang);
+                return interaction.update({
+                    embeds: [ fn.makeEmbed({ title: locale[newLang].commands.setlanguage.successtitle, description: locale[newLang].commands.setlanguage.successdescriptions[0].replace("newlang", locale[newLang].commands.setlanguage.languages[newLang]) }) ],
+                    components: []
+                });
+            } else {
+                fn.db.users.set(interaction.user.id, newLang);
+                interaction.update({
+                    embeds: [ fn.makeEmbed({ title: locale[newLang].commands.setlanguage.successtitle, description: locale[newLang].commands.setlanguage.successdescriptions[1].replace("newlang", locale[newLang].commands.setlanguage.languages[newLang]) }) ],
+                    components: []
+                });
+            }
+        }
 
-        interaction.message.delete();
-        (await fn.getMessage(interaction.message.channel, interaction.message.reference.messageId)).delete()
-    }
-    if(interaction.customId.startsWith("run_")) {
-        if(interaction.member.user.id !== interaction.customId.split("_")[2]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] }); 
-        interaction.deferUpdate();
-
-        const msg = await fn.getMessage(interaction.message.channel, interaction.message.reference.messageId);
-        const args = msg.content.slice("t!".length).split(" ");
-        const providedCommand = interaction.customId.split("_")[1].toLowerCase();
-        args.shift();
-        
-        const command = client.commands.get(providedCommand);
-
-        if(args.length === 1 && ["help", "h"].includes(args[0])) {
-            msg.content = `t!help ${command.name}`;
-            client.commands.get("help").execute(msg, [ command.name ]);
-        } else command.execute(msg, args, client);
-
-        interaction.message.delete();
-    }
-
-    if(interaction.customId.startsWith("lang")) {
-        const lang = fn.getLang(interaction);
-        const newLang = interaction.values[0];
-
-        if(interaction?.guild) { 
-            if(interaction.member.user.id !== interaction.customId.split("_")[1]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] }); 
-
-            fn.db.guilds.set(interaction?.guild?.id, newLang);
+        if(interaction.customId === "resetlang") {
+            fn.db.users.delete(interaction.user.id);
+    
             return interaction.update({
-                embeds: [ fn.makeEmbed({ title: locale[newLang].commands.setlanguage.successtitle, description: locale[newLang].commands.setlanguage.successdescriptions[0].replace("newlang", locale[newLang].commands.setlanguage.languages[newLang]) }) ],
-                components: []
-            });
-        } else {
-            fn.db.users.set(interaction.user.id, newLang);
-            interaction.update({
-                embeds: [ fn.makeEmbed({ title: locale[newLang].commands.setlanguage.successtitle, description: locale[newLang].commands.setlanguage.successdescriptions[1].replace("newlang", locale[newLang].commands.setlanguage.languages[newLang]) }) ],
+                embeds: [ fn.makeEmbed({ title: "Removed Language", description: "Successfully removed your language, it will now be decided by the server you're running commands from" }) ],
                 components: []
             });
         }
-    }
-    if(interaction.customId === "resetlang") {
-        fn.db.users.delete(interaction.user.id);
-
-        return interaction.update({
-            embeds: [ fn.makeEmbed({ title: "Removed Language", description: "Successfully removed your language, it will now be decided by the server you're running commands from" }) ],
-            components: []
-        });
-    }
-
-    if(interaction.customId.startsWith("typing") || interaction.customId.startsWith("bot")) {
-        if(interaction.member.user.id !== interaction.customId.split("_")[1]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] })
-        const command = client.commands.get(interaction.values[0]);
-
-        interaction.update({ embeds: [ fn.makeEmbed({
-            title: command.name,
-            description: locale[lang].commands[command.name].info.description,
-            fields: [
-                [ locale[lang].commands.help.usage, `\`t!${command.name}${locale[lang].commands[command.name].info?.args ? " " : ""}${locale[lang].commands[command.name].info?.args?.map(a => `[${a}]`) ?? ""}\`` ],
-                [ locale[lang].commands.help.aliases, command?.aliases?.map(a => `\`${a}\``).join(", ") ?? locale[lang].commands.help.none ],
-            ],
-            footer: [ command.category ]
-        }) ] });
+    
+        if(interaction.customId.startsWith("typing") || interaction.customId.startsWith("bot")) {
+            if(interaction.member.user.id !== interaction.customId.split("_")[1]) return interaction.reply({ ephemeral: true, embeds: [ fn.makeError(locale[lang].buttons.authoronly, interaction) ] })
+            const command = client.commands.get(interaction.values[0]);
+    
+            interaction.update({ embeds: [ fn.makeEmbed({
+                title: command.name,
+                description: locale[lang].commands[command.name].info.description,
+                fields: [
+                    [ locale[lang].commands.help.usage, `\`t!${command.name}${locale[lang].commands[command.name].info?.args ? " " : ""}${locale[lang].commands[command.name].info?.args?.map(a => `[${a}]`) ?? ""}\`` ],
+                    [ locale[lang].commands.help.aliases, command?.aliases?.map(a => `\`${a}\``).join(", ") ?? locale[lang].commands.help.none ],
+                ],
+                footer: [ command.category ]
+            }) ] });
+        }
     }
 });
 
