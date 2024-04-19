b2f1eba87f6c49ff8b28a6f3833c790a1623eb60
TheAnnoying <drork2018@gmail.com>
2024-04-19 19:54:27 +0300

    chore: updated ping to a more interaction approach for slash commands

--- slash/ping.js
+++ slash/ping.js
@@ -4,12 +4,12 @@ export default {
         const lang = fn.getLang(interaction);
 
         const before = Date.now();
-        const ping = await interaction.reply({ embeds: [ fn.makeEmbed({
+        await interaction.reply({ embeds: [ fn.makeEmbed({
             title: locale[lang].commands.ping.titles[0],
             field: [ locale[lang].commands.ping.fields[0], `${client.ws.ping}ms` ],
         }) ], fetchReply: true });
 
-        ping.edit({ embeds: [ fn.makeEmbed({
+        interaction.editReply({ embeds: [ fn.makeEmbed({
             title: locale[lang].commands.ping.titles[1],
             fields: [
                 [ locale[lang].commands.ping.fields[0], `${client.ws.ping}ms` ],
